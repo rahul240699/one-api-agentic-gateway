0a6abb1e48e9e4b28bd1c6788e08ceb0ba4e9aec
Rahul Sohandani <rsohandani@gmail.com>
2026-06-01 21:08:05 -0400

    render-vercel-deployment-3

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bash start.sh
+web: gunicorn app.main:app -w 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-exec uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
+exec gunicorn app.main:app -w 1 -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${PORT:-10000}"
